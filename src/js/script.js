97d4564fa20b0b1ef4791c902390ca53435f478e
Michal Andronik <michal.androni@gmail.com>
2021-09-17 16:29:26 +0200

    last fix Amount Widget class

--- src/js/script.js
+++ src/js/script.js
@@ -233,7 +233,7 @@
       const thisWidget = this;
 
       thisWidget.getElements(element);
-      thisWidget.setValue(thisWidget.input.value);
+      thisWidget.setValue(settings.amountWidget.defaultValue);
       thisWidget.initActions();
 
       //console.log('AmountWidget', thisWidget);
